d5288df0ae55cb070d4505e7b75dabe9d929f550
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2024-12-16 20:21:26 +0330

    fix img dimentions-2

--- inc/widget/i8_show_posts_pro/content/4sogh_box.php
+++ inc/widget/i8_show_posts_pro/content/4sogh_box.php
@@ -53,7 +53,7 @@ if ($hide_title != 'on') {
         </div>
         <div class="col-24 col-lg-14 overflow-hidden pe-0 ps-lg-3 px-0">
           <a href="<?php the_permalink(); ?>" class="image_frame">
-            <?php echo i8_the_thumbnail('i8-500-280', 'hover multi-item-thumb w-100 i8-img-fit', '', true, '', false, true); ?>
+            <?php echo i8_the_thumbnail('i8-500-280', 'hover multi-item-thumb w-100 i8-img-fit', array("width" => '100%', "height" => 'auto'), true, '', false, true); ?>
           </a>
         </div>
       </div>
